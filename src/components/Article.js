ee323279200b0ccdfc80b82f7faf1693f8acef08
SallyMulupi <mulupisally2gmail.com>
2022-07-15 22:46:43 +0300

    declared emoji

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,3 +1,9 @@
 import React from 'react'
 
 function makeEmojiList(minutes) {
+    const interval = minutes < 30 ? 5 : 10;
+    const emoji = minutes < 30 ? "☕️" : "🍱";
+    let emojis = "";
+    for (let i = 0; i < minutes; i += interval) {
+      emojis += emoji;
+    }
