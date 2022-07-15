3d8c32768207f689a9d55f0bb82ec6a35c26e721
SallyMulupi <mulupisally2gmail.com>
2022-07-15 22:39:20 +0300

    add function app

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,11 @@
 import React from "react";
 import blogData from "../data/blog";
+import Header from "./Header";
+import About from "./About";
+import ArticleList from "./ArticleList";
+import posts from "./ArticleList";
+console.log(posts);
+function App(){}
 
-console.log(blogData);
-
-function App() {
-  return (
-    <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
-    </div>
-  );
-}
 
 export default App;
