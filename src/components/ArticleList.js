735825d7d2b9a3254a061111cca36db90e794fa8
SallyMulupi <mulupisally2gmail.com>
2022-07-15 22:54:11 +0300

    add const header

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,3 +1,13 @@
 import React from 'react'
 import Article from './Article';
 const ArticleList = ({ posts }) => {
+    return (
+        <main>
+          {posts.map(post => <Article key={posts.id} title={posts.title} date={posts.date} preview={posts.preview} minutes={posts.minutes}/>)}
+        </main>
+      )
+    }
+    
+    
+    
+    export default ArticleList;
