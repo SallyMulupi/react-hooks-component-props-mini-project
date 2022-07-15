61f18166212f7368f9d49dad815c27ad1038021d
SallyMulupi <mulupisally2gmail.com>
2022-07-15 22:42:08 +0300

    add articlelist posts

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,7 @@ function App(){
     <div className="App">
       <Header name={blogData.name}/>
       <About  image={blogData.image} about={blogData.about}/>
+      <ArticleList posts={blogData.posts}/>
     </div>
   )
 }
